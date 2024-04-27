950108c259f8918c47aa9ff4885156505947d09d
Jesus W Sanchez <jwsanchez@outlook.es>
2024-04-27 12:17:34 -0500

    #[00] - [java]

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/jwsanchez.java
@@ -0,0 +1,25 @@
+/*
+* Esto es un comentario de varias lineas (Bloque) en Java,
+* a continuación la url de la documentación
+* https://docs.oracle.com/en/java/
+* */
+// Comentario para una sola linea
+
+
+public class jwsanchez {
+    public static void main(String[] args) { //Metodo principal de java
+        int variable_entera;
+        final int constante_entera; //Constante entera solo se prodrá utilizar dentro del metedo main
+        String cadena = "¡Hola Java!";
+        System.out.println(cadena);
+    }
+
+    public void variables(){
+        int entero = 15;
+        float decimales = 1.5f;
+        char caracter = 'r';
+        String cadena = "¡Hola Java!";
+        boolean booleano = true;
+    }
+
+}
